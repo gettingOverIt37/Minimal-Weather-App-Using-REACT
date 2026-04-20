846bb9c166b936503860f42e1fd3a57bab9e8c2c
gettingOverIt37 <aniket2007195@gmail.com>
2026-04-21 00:32:45 +0530

    Publish weather app through docs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Minimal-Weather-App-Using-REACT/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Minimal-Weather-App-Using-REACT/docs/' : '/',
   plugins: [react()],
-})
+}))
